7b92747efadaed5119c8bab182e0af9a62ec12e9
a-poor <45295232+a-poor@users.noreply.github.com>
2021-08-16 20:51:10 -0700

    Updating godb types

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 _created by Austin Poor_
 
+## Installation
+
+
+## Quick Start
+
+
+## File Format
+
+Information about the `.godb` file format.
+
+### Header Format
+
+
+### Row Format
---
+++ godb.go
@@ -0,0 +1,69 @@
+package main // godb
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type DataType int
+
+const (
+	TypeString DataType = iota
+	TypeInt
+	TypeFloat
+	TypeBool
+	TypeCategory
+)
+
+type Row struct {
+	F string `godb:"nullable"`
+}
+
+type Column struct {
+	Index      int
+	Name       string
+	Type       DataType
+	IsNullable bool
+}
+
+func (c Column) ToBinary() []byte {
+	return nil
+}
+
+type DB struct {
+	Filename   string
+	Columns    []Column
+	Categories map[string]map[uint8]string
+}
+
+func (db *DB) createHeader() []byte {
+	return make([]byte, 0)
+}
+
+func (db *DB) loadHeader() error {
+	return nil
+}
+
+/*
+ *
+ *
+ */
+func NewDB(filename string, row interface{}) (*DB, error) {
+	return &DB{Filename: filename}, nil
+}
+
+func getColsFromStruct(t interface{}) ([]Column, error) {
+	return nil, nil
+}
+
+func main() {
+	r := Row{}
+	rt := reflect.TypeOf(r)
+	for i := 0; i < rt.NumField(); i++ {
+		f := rt.Field(i)
+		fmt.Println(f.Name)
+		for k, v := range f.Tag {
+			fmt.Printf("    %s = %s\n", string(k), v)
+		}
+	}
+}
